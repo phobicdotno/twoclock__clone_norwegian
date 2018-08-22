de3a20d7ae9135dfcd6b3c1f8061ec3fc6840950
phobicdotno <spamme.post@gmail.com>
2018-08-23 00:07:46 +0200

    Added Randomhat
    Added randomhat to generate pseudo random colors
    http://playground.arduino.cc/Main/RandomHat

---
+++ randomhat.h
@@ -0,0 +1,35 @@
+/* RandomHat
+  Paul Badger 2007 - updated for Teensy compile 2017
+  choose one from a hat of n consecutive choices each time through loop
+  Choose each number exactly once before reseting and choosing again
+*/
+
+#define randomHatStartNum 0  // starting number in hat
+#define randomHatEndNum 255    // ending number in hat - end has to be larger than start  
+#define numberInHat (randomHatEndNum - randomHatStartNum) + 1
+
+int randomHat() {
+  int thePick;    //this is the return variable with the random number from the pool
+  int theIndex;
+  static int currentNumInHat = 0;
+  static int randArray[numberInHat];
+
+  if  (currentNumInHat == 0) {                  // hat is emply - all have been choosen - fill up array again
+    for (int i = 0 ; i < numberInHat; i++) {    // Put 1 TO numberInHat in array - starting at address 0.
+      if (randomHatStartNum < randomHatEndNum) {
+        randArray[i] = randomHatStartNum + i;
+      }
+    }
+    currentNumInHat = abs(randomHatEndNum - randomHatStartNum) + 1;   // reset current Number in Hat
+    // Serial.print(" hat is empty ");
+    // if something should happen when the hat is empty do it here
+  }
+
+  theIndex = random(currentNumInHat);                  //choose a random index from number in hat
+  thePick = randArray[theIndex];
+  randArray[theIndex] =  randArray[currentNumInHat - 1]; // copy the last element in the array into the the empty slot
+  //                                                     // as the the draw is random this works fine, and is faster
+  //                                                     // the previous version. - from a reader suggestion on this page
+  currentNumInHat--;    // decrement number in hat
+  return thePick;
+}
--- twoclock_clone_norwegian.ino
+++ twoclock_clone_norwegian.ino
@@ -7,6 +7,11 @@
 #include <Adafruit_NeoPixel.h>
 #include <Time.h>
 #include <TimeLib.h>
+#include "randomhat.h"
+
+#ifndef PSTR
+#define PSTR // Make Arduino Due happy
+#endif
 
 #define PIN 6
 
@@ -29,12 +34,8 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
+// 8x8 tall matrix
 
-// Example for NeoPixel Shield.  In this application we'd like to use it
-// as a 5x8 tall matrix, with the USB port positioned at the top of the
-// Arduino.  When held that way, the first pixel is at the top right, and
-// lines are arranged in columns, progressive order.  The shield uses
-// 800 KHz (v2) pixels that expect GRB color data.
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
                             NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
                             NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
@@ -43,7 +44,6 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
 const uint16_t colors[] = {
   matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
 };
-
 
 // Color definitions
 #define BLACK    0x0000
@@ -54,6 +54,8 @@ const uint16_t colors[] = {
 #define MAGENTA  0xF81F
 #define YELLOW   0xFFE0
 #define WHITE    0xFFFF
+#define RANDOM   RGB_random()
+
 
 void setup() {
   matrix.begin();
@@ -62,12 +64,12 @@ void setup() {
   matrix.setTextColor(colors[0]);
   Serial.begin(9600);      // open the serial port at 9600 bps:
   setTime(23, 05, 0, 21, 8, 18);
-
 }
 
 int x    = matrix.width();
 int pass = 0;
 int d1 = 500;
+
 
 void loop() {
 /*  Serial.print(hour());
@@ -76,105 +78,92 @@ void loop() {
   Serial.print(":");
   Serial.println(second());
 */
+
+delay(1000);
   matrix.fillScreen(0);
   minutes();
   hours();
+//  phobic();
   matrix.show();
 } // loop
 
-
+uint16_t RGB_random(){
+  int randR = randomHat();
+  int randG = randomHat();
+  int randB = randomHat();
+  return matrix.Color(randR, randG, randB);
+  }
 
 void phobic() {
-  matrix.drawFastHLine(5, 0, 3, RED);
-  matrix.drawFastHLine(5, 1, 3, RED);
+  matrix.drawFastHLine(5, 0, 3, RANDOM);
+  matrix.drawFastHLine(5, 1, 3, RANDOM);
+  matrix.drawFastHLine(3, 0, 2, RANDOM);  // Row#3, Line#0
 }
+
 void fem_() {
-  matrix.drawFastHLine(0, 0, 3, RED);
+  matrix.drawFastHLine(0, 0, 3, RANDOM);
 }
 void ti_() {
-  matrix.drawFastHLine(3, 0, 2, BLUE);  // Row#3, Line#0
+  matrix.drawFastHLine(3, 0, 2, RANDOM);  // Row#3, Line#0
 }
 void kvart_() {
-  matrix.drawFastHLine(0, 1, 5, YELLOW);  // Row#0, Line#1
+  matrix.drawFastHLine(0, 1, 5, RANDOM);  // Row#0, Line#1
 }
 void halv_() {
-  matrix.drawFastHLine(0, 3, 4, GREEN); 
+  matrix.drawFastHLine(0, 3, 4, RANDOM); 
 }
 void pa_() {
-  matrix.drawFastHLine(0, 2, 2, GREEN);
+  matrix.drawFastHLine(0, 2, 2, RANDOM);
 }
 void over_() {
-  matrix.drawFastHLine(3, 2, 4, CYAN);
+  matrix.drawFastHLine(3, 2, 4, RANDOM);
 }
 
 void ett() {
-  matrix.drawFastHLine(5, 6, 3, MAGENTA);
+  matrix.drawFastHLine(5, 6, 3, RANDOM);
 }
 void to() {
-  matrix.drawFastHLine(0, 5, 2, WHITE);
+  matrix.drawFastHLine(0, 5, 2, RANDOM);
 }
 void tre() {
-  matrix.drawFastVLine(0, 5, 3, RED);
+  matrix.drawFastVLine(0, 5, 3, RANDOM);
 }
 void fire() {
-  matrix.drawFastHLine(0, 4, 4, BLUE);
+  matrix.drawFastHLine(0, 4, 4, RANDOM);
 }
 void fem() {
-  matrix.drawFastHLine(5, 3, 3, YELLOW);
+  matrix.drawFastHLine(5, 3, 3, RANDOM);
 }
 void seks() {
-  matrix.drawFastHLine(4, 4, 4, GREEN);
+  matrix.drawFastHLine(4, 4, 4, RANDOM);
 }
 void sju() {
-  matrix.drawFastHLine(4, 5, 3, CYAN);
+  matrix.drawFastHLine(4, 5, 3, RANDOM);
 }
 void atte() {
-  matrix.drawFastHLine(2, 6, 4, MAGENTA);
+  matrix.drawFastHLine(2, 6, 4, RANDOM);
 }
 void ni() {
-  matrix.drawFastHLine(6, 7, 2, WHITE);
+  matrix.drawFastHLine(6, 7, 2, RANDOM);
 }
 void ti() {
-  matrix.drawFastVLine(7, 6, 2, RED);
+  matrix.drawFastVLine(7, 6, 2, RANDOM);
 }
 void elleve() {
-  matrix.drawFastHLine(0, 7, 6, BLUE);
+  matrix.drawFastHLine(0, 7, 6, RANDOM);
 }
 void tolv() {
-  matrix.drawFastHLine(0, 5, 4, YELLOW);
+  matrix.drawFastHLine(0, 5, 4, RANDOM);
 }
 void l_dot() {
-  matrix.drawFastHLine(0, 7, 1, GREEN);
+  matrix.drawFastHLine(0, 7, 1, RANDOM);
 }
 void r_dot() {
-  matrix.drawFastHLine(7, 7, 1, CYAN);
+  matrix.drawFastHLine(7, 7, 1, RANDOM);
 }
 
-void hours() {
-  // HOURS
-  if (hourFormat12() == 1) {
-    if (minute() < 30 ) {
-      ett();//   Serial.println("Mindre enn tretti");
-    }
-    else {
-      to();//  Serial.println("Mer enn tretti");
-    }
-  } // if
-  if (hourFormat12() == 2) { if (minute() < 30 ) { to(); }  else { tre(); }}
-  if (hourFormat12() == 3) { if (minute() < 30 ) { tre(); }  else { fire(); }}
-  if (hourFormat12() == 4) { if (minute() < 30 ) { fire(); }  else { fem(); }}
-  if (hourFormat12() == 5) { if (minute() < 30 ) { fem(); }  else { seks(); }}
-  if (hourFormat12() == 6) { if (minute() < 30 ) { seks(); }  else { sju(); }}
-  if (hourFormat12() == 7) { if (minute() < 30 ) { sju(); }  else { atte(); }}
-  if (hourFormat12() == 8) { if (minute() < 30 ) { atte(); }  else { ni(); }}
-  if (hourFormat12() == 9) { if (minute() < 30 ) { ni(); }  else { ti(); }}
-  if (hourFormat12() == 10) { if (minute() < 30 ) { ti(); }  else { elleve(); }}
-  if (hourFormat12() == 11) { if (minute() < 30 ) { elleve(); }  else { tolv(); }}
-  if (hourFormat12() == 12) { if (minute() < 30 ) { tolv(); }  else { ett(); }}
-
-
-}
 // MINUTES
+
 void minutes() {
   if (minute() > 3 && minute() <= 7) {
     fem_(); over_();   Serial.println("Ca fem over");
@@ -220,7 +209,29 @@ void minutes() {
     fem_(); pa_();  Serial.println("Ca fem på");
     }
 }
+void hours() {
+  if (hourFormat12() == 1) {
+    if (minute() < 30 ) {
+      ett();//   Serial.println("Mindre enn tretti");
+    }
+    else {
+      to();//  Serial.println("Mer enn tretti");
+    }
+  } // if
+  if (hourFormat12() == 2) { if (minute() < 30 ) { to(); }  else { tre(); }}
+  if (hourFormat12() == 3) { if (minute() < 30 ) { tre(); }  else { fire(); }}
+  if (hourFormat12() == 4) { if (minute() < 30 ) { fire(); }  else { fem(); }}
+  if (hourFormat12() == 5) { if (minute() < 30 ) { fem(); }  else { seks(); }}
+  if (hourFormat12() == 6) { if (minute() < 30 ) { seks(); }  else { sju(); }}
+  if (hourFormat12() == 7) { if (minute() < 30 ) { sju(); }  else { atte(); }}
+  if (hourFormat12() == 8) { if (minute() < 30 ) { atte(); }  else { ni(); }}
+  if (hourFormat12() == 9) { if (minute() < 30 ) { ni(); }  else { ti(); }}
+  if (hourFormat12() == 10) { if (minute() < 30 ) { ti(); }  else { elleve(); }}
+  if (hourFormat12() == 11) { if (minute() < 30 ) { elleve(); }  else { tolv(); }}
+  if (hourFormat12() == 12) { if (minute() < 30 ) { tolv(); }  else { ett(); }}
 
+
+}
 // void drawPixel(uint16_t x, uint16_t y, uint16_t color);
 // void drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color);
 // For horizontal or vertical lines, there are optimized line-drawing
